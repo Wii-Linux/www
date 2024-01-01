7397f3cf13e4b48b49ef78379f7bbcfa073f9e61
Techflash <72118300+techflashYT@users.noreply.github.com>
2023-12-31 17:05:37 -0700

    Internet channel approved blog

---
+++ blog/12-31-2023_internet_channel_approved.php
@@ -0,0 +1,24 @@
+<?php
+$title = "wii-linux.org is now Wii Internet Channel Approved!";
+$date = "Dec 31, 2023";
+$author = "Techflash";
+require '___header.php';
+global $noLoad;
+if ($noLoad === true) {
+	return;
+}
+?>
+
+I've tested the site with loading it on an actual Wii with
+the Opera 9.0-based Internet Channel.  It needed a bit of work
+to function properly, but now I have it working with a few
+special hacks specific to the Wii.
+
+<video class="video-js" id="my-video">
+  <source src="/blog/media/internet_channel_approved.mp4" type="video/mp4">
+  <source src="/blog/media/internet_channel_approved.webm" type="video/webm">
+  Your browser does not support HTML5 video. &nbpsp;Trying to load an Adobe Flash version for ancient browsers.
+  <script src="/video.js">
+    const player = videojs(document.querySelector('.video-js'));
+  </script>
+</video>
