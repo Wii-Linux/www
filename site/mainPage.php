1f1d3ab0375d80cbd9b849a5ad921f77ecda860b
techflashYT <officialTechflashYT@gmail.com>
2024-10-26 04:23:13 +0000

    forgot to push the new website changes

--- site/mainPage.php
+++ site/mainPage.php
@@ -48,7 +48,6 @@ What works and works well:
 - Terminal graphics output in either 480i/480p
 - Booting from MINI
 - USB Gecko (serial console in GameCube memory card ports)
-- KGDB - Linux kernel deugger, works over USB Gecko
 - USB Keyboards
 - Using a portion of <a href=https://wiibrew.org/wiki/Memory_map>MEM2</a> to boost the total usable memory count from 24MB to 74MB
 - Swapping to any USB/SD disk to increase the total ""memory"" count to theoretically any number
@@ -58,10 +57,12 @@ What works and works well:
 
 What works if you ignore the obvious problems:
 - Booting from HBC (Homebrew Channel) under Starlet IOS
+- Anything that uses Java (memory issues, and INCREDIBLY slow)
 
 What doesn't work at all:
 - The DVD driver
-- Anything that uses Java (memory issues, refuses to swap, but in fact now it crashes)
+- Wayland support (requires a Direct Rendering Manager / "DRM" driver for the Wii's GPU)
+- KGDB - Linux kernel deugger, works over USB Gecko (previously worked)
 
 What's not known yet:
 - When did IOS-based SD Card support break?
@@ -71,17 +72,17 @@ What's not known yet:
 Links
 -----------
 - <a href="https://discord.com/invite/D9EBdRWzv2">Official Discord Server</a>
-- <a href="/installation_guide">Install Guide (TBD, ask Techflash until it's done!)</a>
+- <a href="/installation_guide">Install Guide (not yet finalized, WIP copy is in the Discord)</a>
 - <a href="/blog">Wii Linux developers blog</a>  See progress updates and generally what's going on here.
 - <a href="/kernel-docs.zip">Linux 4.5 Documentation (HTML)</a>, built, and then assembled as a ZIP archive.
-- Latest build's rootfs (.tar.xz, extract to the root of your SD/USB device's ext4 partition) <a href="/latest_full.tar.xz">here.</a>
-- Latest build's (kernel 4.5.0) SD Card files (.tar.gz, extract to the root of your SD Card's FAT32 partition) <a href="/latest_4.5_sd_files.tar.gz">here.</a>
+- <b>NEW!</b> Latest build's ArchPOWER rootfs (.tar.gz, extract to the root of your SD/USB device's ext4 partition) <a href="/files/wii_linux_rootfs_archpower-latest.tar.gz">here.</a>
+- <b>NEW!</b> Latest build's ArchPOWER SD Card files (.tar.gz, extract to the root of your SD Card's FAT32 partition) <a href="/files/wii_linux_sd_files_archpower-latest.tar.gz">here.</a>
 
 Specialized (already included in the above)
 -----------
-- Latest build's (kernel 4.5.0) loader.img (raw file, put in gumboot on your SD Card's FAT32 partition) <a href="/latest-loader-4.5.img">here.</a>
-- Latest build's kernel (4.5.0) modules (.tar.gz, need these for Wi-Fi primarily, extract to rootfs) <a href="/latest_4.5_modules.tar.gz">here.</a>
-- Latest build's raw kernel (4.5.0), put this as a file named v4_5_0.krn in the gumboot directory on your SD Card's FAT32 partition <a href="/latest-kernel_4.5.elf">here.</a>
+- Latest build's (kernel 4.5.0) loader.img (raw file, put in wiilinux on your SD Card's FAT32 partition) <a href="/files/wii_linux_loader_v4_5_0-latest.img">here.</a>
+- Latest build's kernel (4.5.0) modules (.tar.gz, need these for Wi-Fi primarily, extract to rootfs) <a href="/files/wii_linux_modules_v4_5_0-latest.tar.gz">here.</a>
+- Latest build's raw kernel (4.5.0), put this as a file named v4_5_0.krn in the wiilinux directory on your SD Card's FAT32 partition <a href="/files/wii_linux_kernel_v4_5_0-latest.elf">here.</a>
 - Wi-Fi Firmware (.tar.gz.  extract to the root of your USB disk) <a href="/openfwwf-5.2-bin.tar.gz">here.</a>  See the <a href="/openfwwf-5.2-LICENSE.txt">LICENSE</a>
 
 
@@ -89,17 +90,14 @@ In-Development versions
 It's highlight likely that these versions will have serious problems.
 It's recommended to NOT use these.
 --------------
-- Latest build's (kernel 4.20.0) loader.img (raw file, put in gumboot on your SD Card's FAT32 partition) <a href="/latest-loader_dev.img">here.</a>
-- Latest build's kernel (4.20.0) modules (.tar.gz, need these for Wi-Fi primarily, extract to rootfs) <a href="/latest_dev_modules.tar.gz">here.</a>
-- Latest build's raw kernel (4.20.0), put this as a file named v4_20_0.krn in the gumboot directory on your SD Card's FAT32 partition <a href="/latest-kernel_dev.elf">here.</a>
+- None for now!  Check back later!
 
 Legacy versions
 You shouldn't need these, but they are kept around for historical reasons
 --------------
-- Latest kernel 4.4.302 SD Card files (.tar.gz, extract to the root of your SD Card's FAT32 partition) <a href="/latest_4.4.302_sd_files.tar.gz">here.</a>
-- Latest kernel (4.4.302) modules (.tar.gz, need these for Wi-Fi primarily, extract to rootfs) <a href="/latest_4.4.302_modules.tar.gz">here.</a>
-- Latest build's raw kernel (4.4.302), put this as a file named v4_4_302.krn in the gumboot directory on your SD Card's FAT32 partition <a href="/latest-kernel_4.4.302.elf">here.</a>
 - <a href="/archive">The archive</a> - really old versions, and superseded releases of the current versions
+- <a href="https://archive.org/details/wii-linux-backup_202409">archive #1</a> of old pre-server-crash releases of Wii Linux - by Selim
+- <a href="http://pretzels.onthewifi.com/files/wii_linux/">archive #2</a> of old pre-server-crash releases of Wii Linux - by Tech64
 
 
 Developers
@@ -129,10 +127,18 @@ Developers
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="https://github.com/ThatsItForTheOtherOne"><img <?img("github");?> alt="GitHub Icon"></a>
 <pre>
+- Alexander Baldeck / "kth5"
+    - Creator of ArchPOWER
+    - Helps fix bugs from time to time
+    - Allows Techflash to use a VM on a POWER9 system for building ArchPOWER packages
+</pre>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="https://github.com/kth5"><img <?img("github");?> alt="GitHub Icon"></a>
+<pre>
 - "Foxlet"
     - Main Void Linux handler
     - Expert at all things userspace
-    - Runs the Void Linux PPC project, which is used by us, and we actively help with
+    - Runs the Void Linux PPC project, which was previously used by us, and we actively help with
     - Showed us how we can get more than an old Gentoo tarball from 2010 working
 </pre>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
