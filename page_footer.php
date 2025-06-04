3c7deb946a23584818ec1c93cde3362295b2afaf
techflashYT <officialTechflashYT@gmail.com>
2025-06-04 20:52:46 +0000

    link to site about using basic html

--- page_footer.php
+++ page_footer.php
@@ -1,5 +1,7 @@
 <hr>
 <p align="center" style="font-family: monospace">
+This site is <a href=https://justfuckingusehtml.com/>proudly written in basic HTML and near-zero CSS</a><br>
+<br>
 © Copyright 2024-2025 Techflash.  Some rights reserved<br>
 See <a href="/legal">legal disclaimers, privacy policy, copyright, and trademark notices</a>
 </p>
