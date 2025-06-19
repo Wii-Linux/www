cb76a2c5f25aedc4ad02d6d678cfde9513718c1e
techflashYT <officialTechflashYT@gmail.com>
2025-06-19 07:40:03 +0000

    fix typo

--- site/blog/6-19-2025_dev_build_boot_stack.php
+++ site/blog/6-19-2025_dev_build_boot_stack.php
@@ -13,7 +13,7 @@ Welcome, developers!  There's recently been a <b>major build workflow change, th
 
 <b>For anyone making custom builds, this announcement is for you!</b>
 The build-stack and boot-stack have undergone massive changes, that affect how, and what, you build to get the kernel and boot menu.
-You can check the commit logs on GitHub (<a href="https://github.com/Wii-Linux/boot-stack/compare/1c1013549c6e...cd7815d5c924">boot-stack</a>, <a href="https://github.com/Wii-Linux/build-stack/compare/4535046aac78...dd62bd6a9119">boot-stack</a>) for the actual code changes, but here's the cliff notes:
+You can check the commit logs on GitHub (<a href="https://github.com/Wii-Linux/boot-stack/compare/1c1013549c6e...cd7815d5c924">boot-stack</a>, <a href="https://github.com/Wii-Linux/build-stack/compare/4535046aac78...dd62bd6a9119">build-stack</a>) for the actual code changes, but here's the cliff notes:
 - The Internal Loader has been <b>removed</b>.  If you were relying on that to exist (perhaps making modifications to it), it doesn't anymore.
 - The Loader-Img (Boot Menu) has now <b>included in the kernel</b>.  If you were relying on it to be a `.ldr` file on the SD Card, it isn't anymore.
 - `/run/boot_part` can now <b>fail to mount gracefully</b> in the loader-img, you can't rely on it to always exist.  You can check  `if [ "$noBootPart" = "true" ]` to see if it failed to mount or not.
