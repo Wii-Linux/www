b0545400c598ab4d261182c88d9a45e68ef2c988
techflashYT <officialTechflashYT@gmail.com>
2025-07-19 14:01:30 +0000

    Add branding and usage guidelines

---
+++ site/branding.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+<title>Wii Linux - Branding</title>
+<?php globalHeadStuff(); ?>
+</head>
+<body>
+<pre>
+Wii Linux - Branding
+--------------------
+<a href="/">Back to main page</a>
+
+Wii-Linux Main Logo, Transparent Background:
+<img src="/icons/branding/main-logo-transparent.png" alt="Wii-Linux Main Logo, Transparent Background">
+
+Wii-Linux Main Logo, White Background:
+<img src="/icons/branding/main-logo-white.png" alt="Wii-Linux Main Logo, White Background">
+
+"Wii-Linux 25" Concept, Wallpaper (1920x1080):
+<img src="/icons/branding/wl25-1080p.png" alt="&quot;Wii-Linux 25&quot; Concept, Wallpaper (1920x1080)">
+
+"Wii-Linux 25" Concept, Wallpaper (640x480):
+<img src="/icons/branding/wl25-480p.png" alt="&quot;Wii-Linux 25&quot; Concept, Wallpaper (640x480)">
+
+"Wii-Linux 25" Concept, DVD Box Art:
+<img src="/icons/branding/wl25-dvd.png" alt="&quot;Wii-Linux 25&quot; Concept, DVD Box Art">
+
+Wii-Linux Switch Companion, Boot Splash:
+<img src="/icons/branding/swcomp-splash.png" alt="Wii-Linux Switch Companion, Boot Splash">
+
+Wii-Linux 88x31 badge:
+<img src="/icons/branding/88x31.png" alt="Wii-Linux 88x31 badge">
+
+You are permitted to use the above branding, provided that you adhere to the Wii-Linux Branding Usage Guidelines, as described below.
+
+Wii-Linux Branding Usage Guidelines
+-----------------------------------
+These Branding Usage Guidelines ("Guidelines") outline the permitted and prohibited uses of the Wii-Linux name, logo, and other associated branding elements (collectively, the “Branding”).
+Use of the Branding signifies your agreement to comply with these Guidelines.
+These Guidelines may be updated from time to time.
+Any usage of the Branding must adhere strictly to the following terms.
+
+Permitted Uses
+--------------
+Subject to the terms and conditions outlined herein, you MAY use the Branding to:
+
+1. Refer to the Wii-Linux project as a whole, including in educational, editorial, or descriptive contexts;
+2. Signify or depict Wii-Linux's open-source code, tools, libraries, or services, provided such use clearly indicates that Wii-Linux is the source and not your own creation or service.
+
+Prohibited Uses
+---------------
+Unless you have received express, written permission from the Wii-Linux team or its authorized representatives, you MAY NOT:
+
+1. Use the Branding in a manner that falsely suggests affiliation with, sponsorship by, or endorsement from the Wii-Linux project;
+2. Represent yourself, your organization, or your services as the Wii-Linux project, or as owning or operating any official Wii-Linux services or code;
+3. Use the Branding for any commercial purpose, including but not limited to selling products or services, fundraising, or advertising;
+4. Claim ownership of, or attempt to register, any trademarks, logos, domain names, or other branding elements that are identical or confusingly similar to the Wii-Linux Branding;
+5. Modify the Branding or use altered versions of the logo or name;
+6. Use the Branding in any way not explicitly allowed in the “Permitted Uses” section above.
+
+Enforcement
+-----------
+The Wii-Linux team reserves the right to revoke permission to use the Branding at any time, for any reason.
+
+Copyright
+---------
+All Wii-Linux branding is Copyright (C) 2024-2025 Tech64, licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC-BY-NC-SA).
+Some Wii-Linux branding contains the "Tux" character of Linux, which is Copyright (C) 1996 Larry Ewing &lt;lewing@isc.tamu.edu&gt; and The GIMP.
+</pre>
+
--- site/legal.php
+++ site/legal.php
@@ -17,6 +17,7 @@ Wii Linux - Legal
 Wii Linux Website - © Copyright 2024-2025 Techflash.  Some rights reserved.
 This website's source code (PHP) is available under the GNU General Public License, version 2.0.
 This website's content (text, images, video, etc) is available under the Creative Commons "CC BY-NC-SA" license.
+See the dedicated page about our <a href="/branding">branding and usage guidelines</a>.
 
 
 Privacy
