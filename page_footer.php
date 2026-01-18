65b06a06d7956515f94eba7ae1210f3b657b5d54
techflashYT <officialTechflashYT@gmail.com>
2026-01-18 20:57:08 +0000

    footer: update copyright date

--- page_footer.php
+++ page_footer.php
@@ -2,7 +2,7 @@
 <p align="center" style="font-family: monospace">
 This site is <a href=https://justfuckingusehtml.com/>proudly written in basic HTML and near-zero CSS</a><br>
 <br>
-© Copyright 2024-2025 Techflash.  Some rights reserved<br>
+© Copyright 2024-2026 Techflash.  Some rights reserved<br>
 See <a href="/legal">legal disclaimers, privacy policy, copyright, and trademark notices</a>
 </p>
 </body>
